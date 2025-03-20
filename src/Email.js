d3730f72106228d4f7339ff2678e99f827a88b1c
nishanthan0018 <166122270+nishanthan0018@users.noreply.github.com>
2025-03-20 09:49:29 +0530

    Update Email.js

--- src/Email.js
+++ src/Email.js
@@ -2,9 +2,9 @@ import emailjs from "emailjs-com";
 
 // Function to send email using EmailJS
 export const sendBookingEmail = (formData, selectedDestination) => {
-  const serviceID = "service_ew9lv7k"; // Correct Service ID
-  const templateID = "template_b9qc675"; // Correct Template ID
-  const userID = "FYcOEYKB2P4_q5CHm"; // Correct User ID
+  const serviceID = "service_6qrjhpr"; // Correct Service ID
+  const templateID = "template_ln4mng4"; // Correct Template ID
+  const userID = "Ard1qqoJprj-HEjwB"; // Correct User ID
 
   const templateParams = {
     name: formData.name,
